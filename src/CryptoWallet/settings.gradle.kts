cebdb66fb98837b63f8c59ed0bf5c22a0fb679eb
erikbabel <erik.babel@gmail.com>
2024-08-27 13:31:16 -0700

    cleanup settings file

--- src/CryptoWallet/settings.gradle.kts
+++ src/CryptoWallet/settings.gradle.kts
@@ -7,14 +7,6 @@ pluginManagement {
 
         }
     }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id.startsWith("com.google.cloud.tools.appengine")) {
-                useModule("com.google.cloud.tools:appengine-gradle-plugin:${requested.version}")
-            }
-        }
-    }
 }
 
 include (":app")
