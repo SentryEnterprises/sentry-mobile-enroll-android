d18075743fe25ac1dde4df64efbb17ab695972d5
erikbabel <erik.babel@gmail.com>
2024-11-13 15:00:26 -0800

    Formatting code in EnrollScreen.kt

--- src/CryptoWallet/app/src/main/java/com/sentryenterprises/sentry/enrollment/enroll/EnrollScreen.kt
+++ src/CryptoWallet/app/src/main/java/com/sentryenterprises/sentry/enrollment/enroll/EnrollScreen.kt
@@ -53,35 +53,16 @@ import com.sentryenterprises.sentry.sdk.models.NfcActionResult
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EnrollScreen(
-    modifier: Modifier = Modifier,
     nfcViewModel: NfcViewModel,
     onNavigate: (Screen) -> Unit,
 ) {
-
     val progress = nfcViewModel.fingerProgress.collectAsState().value
     val action = nfcViewModel.nfcAction.collectAsState().value
     val actionResult = nfcViewModel.nfcActionResult.collectAsState().value
 
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text("Fingerprint scan")
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = {
-                            nfcViewModel.resetNfcAction()
-                            onNavigate(Screen.EnrollIntro)
-                        },
-                    ) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Back",
-                        )
-                    }
-                },
-            )
+            TopAppBar(nfcViewModel, onNavigate)
         }
     ) { paddingInsets ->
         Column(
@@ -91,209 +72,65 @@ fun EnrollScreen(
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
-            LaunchedEffect(progress) {
-                println("EnrollScreen progress $progress")
-            }
-            LaunchedEffect(action) {
-                println("EnrollScreen progress $action")
-            }
-            LaunchedEffect(actionResult) {
-                println("EnrollScreen actionResult $actionResult")
-            }
-
             when {
-                actionResult?.getOrNull() is NfcActionResult.EnrollFingerprint.Complete -> {
-                    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.checkmark_animation))
-
-                    val animationState by animateLottieCompositionAsState(
-                        iterations = 1,
-                        composition = composition,
-                    )
-
-                    LottieAnimation(
-                        composition = composition,
-                        progress = { animationState },
-                        modifier = Modifier.size(300.dp)
-                    )
-                }
-
-                progress is BiometricProgress.FingerTransition -> {
-                    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.checkmark_animation))
-
-                    val animationState by animateLottieCompositionAsState(
-                        iterations = 1,
-                        composition = composition,
-                    )
-
-                    LottieAnimation(
-                        composition = composition,
-                        progress = { animationState },
-                        modifier = Modifier.size(300.dp)
-                    )
-                }
-
-                action == null -> {
-                    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.attach_card))
-
-                    val animationState by animateLottieCompositionAsState(
-                        iterations = LottieConstants.IterateForever,
-                        composition = composition,
-                    )
-
-                    LottieAnimation(
-                        composition = composition,
-                        progress = { animationState },
-                        modifier = Modifier
-                    )
-                }
-
-                progress is BiometricProgress.Progressing -> {
-                    val (dark, light) = when (progress.currentStep) {
-                        1 -> R.drawable.finger_left_dark to R.drawable.finger_left
-                        2 -> R.drawable.finger_bottom_dark to R.drawable.finger_bottom
-                        3 -> R.drawable.finger_right_dark to R.drawable.finger_right
-                        4 -> R.drawable.finger_top_dark to R.drawable.finger_top
-                        else -> R.drawable.finger_center_dark to R.drawable.finger_center
-                    }
-                    Image(
-                        painter = painterResource(
-                            if (isSystemInDarkTheme()) {
-                                dark
-                            } else {
-                                light
-                            }
-                        ),
-                        contentDescription = "Portion of finger to place",
-                        modifier = Modifier.size(200.dp)
-                    )
-                }
-
-                else -> {
-                    Image(
-                        painter = painterResource(
-                            if (isSystemInDarkTheme()) {
-                                R.drawable.finger_center_dark
-                            } else {
-                                R.drawable.finger_center
-                            }
-                        ),
-                        contentDescription = "Portion of finger to place",
-                        modifier = Modifier.size(200.dp)
-                    )
-                }
+                actionResult?.getOrNull() is NfcActionResult.EnrollFingerprint.Complete -> CheckmarkAnimation()
+                progress is BiometricProgress.FingerTransition -> CheckmarkAnimation()
+                action == null -> AttachCardAnimation()
+                progress is BiometricProgress.Progressing -> FingerGuideImage(progress.currentStep)
+                else -> FingerGuideImage(0)
             }
 
             if (progress is BiometricProgress.Progressing) {
-                val context = LocalContext.current
-                LaunchedEffect(progress.currentStep) {
-                    if (progress.currentStep > 0) {
-                        val mediaPlayer = MediaPlayer.create(
-                            context,
-                            R.raw.ding
-                        )
-                        mediaPlayer.start()
-                    }
-                }
+                PlaySoundPerStep(progress)
+
+                Text(
+                    "Finger ${progress.currentFinger} of 2\n",
+                    fontSize = 25.sp,
+                    modifier = Modifier.padding(top = 30.dp)
+                )
                 val checkboxes =
                     (1..progress.currentStep).joinToString("") {
                         "✅"
                     } + (1..(progress.remainingTouches)).joinToString("") {
                         "◼️"
                     }
-                Text(
-                    "Finger ${progress.currentFinger} of 2\n",
-                    fontSize = 25.sp,
-                    modifier = Modifier.padding(top = 30.dp)
-                )
                 Text(checkboxes, fontSize = 25.sp)
             }
 
             if (action == null && actionResult == null) {
                 Text(
                     modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                    text = "Place your card on a flat, non-metallic surface then place a phone on top leaving sensor accessible for finger print scanning.",
+                    text = "Place your card on a flat, non-metallic surface then place a phone " +
+                            "on top leaving sensor accessible for finger print scanning.",
                     textAlign = TextAlign.Center,
                     fontSize = 17.sp
                 )
             } else if (action == null && actionResult?.getOrNull() is NfcActionResult.EnrollFingerprint) {
                 when (actionResult.getOrNull()) {
                     is NfcActionResult.EnrollFingerprint.Complete -> {
-                        Text(
-                            modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                            text = "Enrollment Finished",
-                            textAlign = TextAlign.Center,
-                            fontWeight = Bold,
-                            fontSize = 23.sp,
-                        )
-                        Text(
-                            modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                            text = "Your fingerprint is now enrolled. Click Ok to continue.",
-                            textAlign = TextAlign.Center,
-                            fontSize = 17.sp
-                        )
-                        SentryButton(text = "Ok") {
-                            nfcViewModel.resetNfcAction()
-                            onNavigate(Screen.GetCardState)
-                        }
+                        CompleteEnrollmentSection(nfcViewModel, onNavigate)
                     }
 
                     is NfcActionResult.EnrollFingerprint.Failed -> {
-                        Text(
-                            modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                            text = "Unexpected error occurred",
-                            textAlign = TextAlign.Center,
-                            fontWeight = Bold,
-                            fontSize = 23.sp,
-                        )
-                        val errorText = if (BuildConfig.DEBUG) {
-                            "Please try again."
-                        } else {
-                            "$progress"
-                        }
-                        Text(
-                            modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                            text = errorText,
-                            textAlign = TextAlign.Center,
-                            fontSize = 17.sp
-                        )
-                        SentryButton(text = "Retry") {
-                            nfcViewModel.resetNfcAction()
-                            nfcViewModel.startNfcAction(NfcAction.EnrollFingerprint)
-                        }
+                        FailedEnrollmentSection(progress, nfcViewModel)
                     }
 
                     else -> error("Unexpected state: $actionResult")
                 }
 
             } else if (action == null && actionResult?.isFailure == true) {
-
-                val errorText = actionResult.exceptionOrNull().getDecodedMessage()
-
-                Text(
-                    modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                    text = errorText,
-                    textAlign = TextAlign.Center,
-                    fontSize = 17.sp
-                )
-                Text(
-                    modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
-                    text = "Please move the phone away from the card to reset, then try again.",
-                    textAlign = TextAlign.Center,
-                    fontSize = 17.sp
-                )
-                SentryButton(
-                    text = "Retry",
-                    onClick = {
-                        nfcViewModel.resetNfcAction()
-                        nfcViewModel.startNfcAction(NfcAction.EnrollFingerprint)
-                    }
-                )
+                UnknownFailureSection(actionResult, nfcViewModel)
             } else {
                 val instructionText = when (progress) {
-                    is BiometricProgress.Progressing -> "Lift your finger and press a slightly different part of the same finger."
-                    is BiometricProgress.Feedback -> "Card status:${progress.status}. Try again with your finger."
-                    is BiometricProgress.FingerTransition -> "Please use your second finger"
+                    is BiometricProgress.Progressing -> "Lift your finger and press a " +
+                            "slightly different part of the same finger."
+
+                    is BiometricProgress.Feedback -> "Card status:${progress.status}. " +
+                            "Try again with your finger."
+
+                    is BiometricProgress.FingerTransition -> "Please use your second " +
+                            "finger."
+
                     null -> "Press your finger to the card to get started."
                 }
                 Text(
@@ -319,7 +156,180 @@ fun EnrollScreen(
                 )
             }
         }
-
-
     }
+}
+
+@Composable
+private fun UnknownFailureSection(
+    actionResult: Result<NfcActionResult>,
+    nfcViewModel: NfcViewModel
+) {
+    val errorText = actionResult.exceptionOrNull().getDecodedMessage()
+
+    Text(
+        modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
+        text = errorText,
+        textAlign = TextAlign.Center,
+        fontSize = 17.sp
+    )
+    Text(
+        modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
+        text = "Please move the phone away from the card to reset, then try again.",
+        textAlign = TextAlign.Center,
+        fontSize = 17.sp
+    )
+    SentryButton(
+        text = "Retry",
+        onClick = {
+            nfcViewModel.resetNfcAction()
+            nfcViewModel.startNfcAction(NfcAction.EnrollFingerprint)
+        }
+    )
+}
+
+@Composable
+private fun CompleteEnrollmentSection(
+    nfcViewModel: NfcViewModel,
+    onNavigate: (Screen) -> Unit
+) {
+    Text(
+        modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
+        text = "Enrollment Finished",
+        textAlign = TextAlign.Center,
+        fontWeight = Bold,
+        fontSize = 23.sp,
+    )
+    Text(
+        modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
+        text = "Your fingerprint is now enrolled. Click Ok to continue.",
+        textAlign = TextAlign.Center,
+        fontSize = 17.sp
+    )
+    SentryButton(text = "Ok") {
+        nfcViewModel.resetNfcAction()
+        onNavigate(Screen.GetCardState)
+    }
+}
+
+@Composable
+private fun FailedEnrollmentSection(
+    progress: BiometricProgress?,
+    nfcViewModel: NfcViewModel
+) {
+    Text(
+        modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
+        text = "Unexpected error occurred",
+        textAlign = TextAlign.Center,
+        fontWeight = Bold,
+        fontSize = 23.sp,
+    )
+    val errorText = if (BuildConfig.DEBUG) {
+        "Please try again."
+    } else {
+        "$progress"
+    }
+    Text(
+        modifier = Modifier.padding(vertical = 16.dp, horizontal = 24.dp),
+        text = errorText,
+        textAlign = TextAlign.Center,
+        fontSize = 17.sp
+    )
+    SentryButton(text = "Retry") {
+        nfcViewModel.resetNfcAction()
+        nfcViewModel.startNfcAction(NfcAction.EnrollFingerprint)
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun TopAppBar(
+    nfcViewModel: NfcViewModel,
+    onNavigate: (Screen) -> Unit
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text("Fingerprint scan")
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    nfcViewModel.resetNfcAction()
+                    onNavigate(Screen.EnrollIntro)
+                },
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back",
+                )
+            }
+        },
+    )
+}
+
+@Composable
+private fun PlaySoundPerStep(progress: BiometricProgress.Progressing) {
+    val context = LocalContext.current
+    LaunchedEffect(progress.currentStep) {
+        if (progress.currentStep > 0) {
+            val mediaPlayer = MediaPlayer.create(
+                context,
+                R.raw.ding
+            )
+            mediaPlayer.start()
+        }
+    }
+}
+
+@Composable
+private fun FingerGuideImage(currentStep: Int) {
+    val (dark, light) = when (currentStep) {
+        1 -> R.drawable.finger_left_dark to R.drawable.finger_left
+        2 -> R.drawable.finger_bottom_dark to R.drawable.finger_bottom
+        3 -> R.drawable.finger_right_dark to R.drawable.finger_right
+        4 -> R.drawable.finger_top_dark to R.drawable.finger_top
+        else -> R.drawable.finger_center_dark to R.drawable.finger_center
+    }
+    Image(
+        painter = painterResource(
+            if (isSystemInDarkTheme()) {
+                dark
+            } else {
+                light
+            }
+        ),
+        contentDescription = "Portion of finger to place",
+        modifier = Modifier.size(200.dp)
+    )
+}
+
+@Composable
+private fun AttachCardAnimation() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.attach_card))
+
+    val animationState by animateLottieCompositionAsState(
+        iterations = LottieConstants.IterateForever,
+        composition = composition,
+    )
+
+    LottieAnimation(
+        composition = composition,
+        progress = { animationState },
+        modifier = Modifier
+    )
+}
+
+@Composable
+private fun CheckmarkAnimation() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.checkmark_animation))
+
+    val animationState by animateLottieCompositionAsState(
+        iterations = 1,
+        composition = composition,
+    )
+
+    LottieAnimation(
+        composition = composition,
+        progress = { animationState },
+        modifier = Modifier.size(300.dp)
+    )
 }
